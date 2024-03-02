4a197bab19e6518a7a4dd6ba9ac80d6dd89a2e19
CSkmj <kuaimingjin@163.com>
2024-03-02 17:22:31 +0800

    element+自动按需引入配置

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
 
+// 添加ElementPlus到项目（按需导入）
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -7,9 +12,16 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
     alias: {
+      // 这里才起到了实质性的路径转换
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
