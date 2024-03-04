077f7638b6a1980b931a4cfcd6ce45c487dde62b
CSkmj <kuaimingjin@163.com>
2024-03-04 12:03:02 +0800

    home-懒加载指令优化

--- src/main.js
+++ src/main.js
@@ -8,7 +8,9 @@ import router from './router'
 
 //初始化引入样式文件
 import '@/styles/common.scss'
-import { useIntersectionObserver } from '@vueuse/core'
+// 全局指令注册
+import { lazyPlugin } from '@/directives'
+
 
 
 
@@ -16,23 +18,8 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(lazyPlugin)
 
 app.mount('#app')
 
-app.directive('img-lazy',{
-    mounted(el,binding){
-        //el:指令绑定的那个元素 img
-        //binding: binding.value 指令等于号后面绑定的表达式的值，图片url
-        console.log(el, binding.value)
-        useIntersectionObserver(
-            el,
-            ([{isIntersecting}]) => {
-                console.log(isIntersecting)
-                if (isIntersecting) {
-                    //进入视口区域
-                    el.src = binding.value
-                }
-            }
-        )
-    }
-})
+
