36539ec122e9d71c7d4bed3d82e8b2998bfa7414
CSkmj <kuaimingjin@163.com>
2024-03-04 21:09:20 +0800

    一级分类-按照业务逻辑拆分函数

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,35 +1,9 @@
 <script setup>
-import { getCategoryAPI } from "@/apis/category";
-import { getBannerAPI } from "@/apis/home";
-import { onMounted, ref } from "vue";
-import { onBeforeRouteUpdate, useRoute } from "vue-router";
-import GoodsItem from "../Home/components/GoodsItem.vue"
-
-const categoryData = ref({});
-const route = useRoute();
-const getCategory = async ( id = route.params.id) => {
-  const res = await getCategoryAPI(id);
-  categoryData.value = res.result;
-};
-
-onMounted(() => getCategory());
-//期望把分类数据接口重新发送
-onBeforeRouteUpdate((to) => {
-  console.log('路由变化了')
-  console.log(to);
-  getCategory(to.params.id)
-})
-
-const bannerList = ref([]);
-
-const getBanner = async () => {
-  const res = await getBannerAPI({
-    distributionSite: "2",
-  });
-  console.log(res);
-  bannerList.value = res.result;
-};
-onMounted(() => getBanner());
+import GoodsItem from '../Home/components/GoodsItem.vue'
+import { useBanner } from './composables/useBanner'
+import { useCategory } from './composables/useCategory'
+const { bannerList } = useBanner()
+const { categoryData } = useCategory()
 </script>
 
 <template>
