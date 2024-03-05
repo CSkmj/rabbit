4196cb2efe25bb56a6f229568d80de1f289da729
CSkmj <kuaimingjin@163.com>
2024-03-05 17:08:53 +0800

    列表适配

--- src/views/Detail/components/DetailHot.vue
+++ src/views/Detail/components/DetailHot.vue
@@ -1,7 +1,20 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { getHotGoodsAPI } from '@/apis/detail'
 import { useRoute } from 'vue-router'
+
+// type适配不同类型热榜数据
+const props = defineProps({
+  type: {
+    type: Number, // 1代表24小时热销榜 2代表周热销榜 3代表总热销榜 可以使用type去适配title和数据列表
+    default: 1
+  }
+})
+const TITLEMAP = {
+  1: '24小时热榜',
+  2: '周热榜', 
+}
+const title = computed(() => TITLEMAP[props.type])
 
 const goodList = ref([])
 const route = useRoute()
@@ -21,7 +34,7 @@ onMounted(() => getHotList())
 
 <template>
   <div class="goods-hot">
-    <h3> 24小时热榜 </h3>
+    <h3>{{ title }}</h3>
     <!-- 商品区块 -->
     <RouterLink :to="`/detail/${item.id}`" class="goods-item" v-for="item in goodList" :key="item.id">
       <img :src="item.picture" alt="" />
