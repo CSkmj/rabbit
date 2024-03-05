df6ad311216e06c6ff80d2bcdc590136ef1e5c20
CSkmj <kuaimingjin@163.com>
2024-03-05 10:45:07 +0800

    二级分类-基础列表渲染

--- src/views/SubCategory/index.vue
+++ src/views/SubCategory/index.vue
@@ -1,7 +1,9 @@
 <script setup>
-import {getCategoryFilterAPI} from '@/apis/category'
+import { getCategoryFilterAPI , getSubCategoryAPI} from '@/apis/category'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+import GoodsItem from '../Home/components/GoodsItem.vue'
+
 
 const categoryData = ref({})
 const route = useRoute()
@@ -10,6 +12,20 @@ const getCategoryData = async () => {
   categoryData.value = res.result
 }
 onMounted(() => getCategoryData())
+
+const goodList = ref([])
+const reqData = ref({
+  categoryId: route.params.id,
+  page: 1,
+  pageSize: 20,
+  sortField: 'publishTime'
+})
+const getGoodList = async () => {
+  const res = await getSubCategoryAPI(reqData)
+  console.log(res)
+  goodList.value = res.result.items
+}
+onMounted(() => getGoodList())
 
 </script>
 
@@ -32,6 +48,7 @@ onMounted(() => getCategoryData())
       </el-tabs>
       <div class="body">
          <!-- 商品列表-->
+        <GoodsItem v-for="goods in goodList" :goods="goods" :key="goods.id" />
       </div>
     </div>
   </div>
