ca3a9ec0cc1d20be54ab90e882f9b4e47d0f2055
CSkmj <kuaimingjin@163.com>
2024-03-06 15:29:18 +0800

    token拦截器携带

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,6 +3,7 @@
 import axios from "axios"
 import 'element-plus/es/components/message/style/css'
 import { ElMessage } from 'element-plus'
+import { useUserStore } from '@/stores/user'
 
 const httpInstance = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -12,6 +13,13 @@ const httpInstance = axios.create({
 //拦截器
 // axios请求拦截器
 httpInstance.interceptors.request.use(config => {
+   // 1. 从pinia获取token数据
+   const userStore = useUserStore()
+   // 2. 按照后端的要求拼接token数据
+   const token = userStore.userInfo.token
+   if (token) {
+     config.headers.Authorization = `Bearer ${token}`
+   }
     return config
   }, e => Promise.reject(e))
   
