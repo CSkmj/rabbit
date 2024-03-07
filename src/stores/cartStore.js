bbcd207bd9491b142aa4e0e03315031e62a2dc6b
CSkmj <kuaimingjin@163.com>
2024-03-07 10:25:16 +0800

    列表购物车的全选功能

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -39,16 +39,28 @@ export const useCartStore = defineStore('cart', () => {
       const item = cartList.value.find((item) => item.skuId === skuId)
       item.selected = selected
     }
+
+    // 全选功能action
+    const allCheck = (selected) => {
+      // 把cartList中的每一项的selected都设置为当前的全选框状态
+      cartList.value.forEach(item => item.selected = selected)
+    }
   
   const allCount = computed(() => cartList.value.reduce((a,c) => a + c.count, 0))
   const allPrice = computed(() => cartList.value.reduce((a,c) => a + c.count * c.price, 0))
+
+    // 是否全选计算属性
+    const isAll = computed(() => cartList.value.every((item) => item.selected))
+
   return {
     cartList,
     allCount,
     allPrice,
     addCart,
     delCart,
-    singleCheck
+    singleCheck,
+    allCheck,
+    isAll
   }
 }, {
   persist: true,
